2c7380967e5cbd7203211f27a8d1047bb085a20f
Udit kumar <udit@updot.co>
2026-02-27 11:52:47 +0530

    Dynamic Routing :Recipe Inner page

--- app/components/RecipeCard.vue
+++ app/components/RecipeCard.vue
@@ -4,6 +4,7 @@ import { type Recipe } from "../../types/types";
 defineProps<{
   recipe: Recipe;
 }>();
+
 </script>
 
 <template>
@@ -32,11 +33,11 @@ defineProps<{
           <span>{{ recipe.rating }} ({{ recipe.reviewCount }})</span>
         </div>
       </div>
-      <button
+      <NuxtLink to = "/recipe/${recipe.id}"
         class="px-4 py-2 text-white self-start bg-dodgeroll-gold rounded-md text-base lg:text-lg cursor-pointer"
       >
         View
-      </button>
+      </NuxtLink>
     </div>
   </div>
 </template>
