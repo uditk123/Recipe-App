61372605ed888f45f037aef536449bdcf6826048
Udit kumar <udit@updot.co>
2026-02-27 16:17:10 +0530

    Tested stores value in contact page

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,10 +1,14 @@
  
   <script setup lang="ts">
- 
+ import { useLikeStore } from '~/stores/likes';
+
+ const Likes = useLikeStore();
+ console.log(" Checking store data , Likes.Like");
  </script>
  <template>
     <div>
      <h1>Contact Us</h1>
+     <h3> Store Values : {{ Likes.Like }}</h3>
     </div>
  </template>
  
