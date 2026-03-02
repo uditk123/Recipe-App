9b6cb3e4111c2aba1beb4aa53a2def9bf237a995
Udit kumar <udit@updot.co>
2026-03-02 16:17:47 +0530

    Pinia state Component

---
+++ app/components/piniaState1.vue
@@ -0,0 +1,20 @@
+<script setup >
+// Importing the store
+import { useLikeStore } from '~/stores/likes';
+
+console.log("store",useLikeStore);
+
+const Likes = useLikeStore();
+</script>
+
+<template>
+    <div>
+        <!-- Fetching store data -->
+       <h2> Total Likes : {{ Likes.Like }}</h2>
+       <!-- Fetching getter -->
+        <h3> Double-Likes : {{ Likes.DoubleLikes }}</h3>
+        <button @click = "Likes.increment">IncreseLikes</button>
+        <br>
+        <button @click = "Likes.decrement">DecreseLikes</button>
+    </div>
+</template>
--- app/pages/piniastatemanagement.vue
+++ app/pages/piniastatemanagement.vue
@@ -1,25 +1,9 @@
-<script setup >
-// Importing the store
-import { useLikeStore } from '~/stores/likes';
 
-console.log("store",useLikeStore);
-
-const Likes = useLikeStore();
-</script>
 
 <template>
     <div>
-        <!-- Fetching store data -->
-       <h2> Total Likes : {{ Likes.Like }}</h2>
-       <!-- Fetching getter -->
-        <h3> Double-Likes : {{ Likes.DoubleLikes }}</h3>
-        <button @click = "Likes.increment">IncreseLikes</button>
-        <br>
-        <button @click = "Likes.decrement">DecreseLikes</button>
+        <PiniaState1 />
     </div>
 </template>
 
 
-<style scoped>
-
-</style>
