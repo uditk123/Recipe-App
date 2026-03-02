a4ff3bda56c7a26a97f8332656f3f04e4679c084
Udit kumar <udit@updot.co>
2026-03-02 16:38:52 +0530

    testing custom : API

---
+++ app/pages/testingApi.vue
@@ -0,0 +1,14 @@
+<script setup >
+
+const { data , error} = await useFetch("/api/testdata")
+console.log("Testing data is ", data.value);
+</script>
+
+<template>
+    <div>
+     <h1>Testing the Custom Api data</h1>
+     <p> MESSAGE IS : {{  data.message }}</p>
+     <P> API HELLO IS : {{ data.hello }}</P>
+    </div>
+</template>
+
