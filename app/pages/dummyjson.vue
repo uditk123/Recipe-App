28ff1b37bc68e83225016ff0a8967cd8288a1042
Udit kumar <udit@updot.co>
2026-03-03 12:14:23 +0530

    Tested the custom Api using dummy json data

---
+++ app/pages/dummyjson.vue
@@ -0,0 +1,23 @@
+  <script setup>
+
+  const { data } = useFetch("http://localhost:3000/api/posts");
+  console.log("APi data is ", data);
+ 
+ </script>
+ 
+ <template>
+    <div v-for = "item in data">
+        <div>
+        <h1> Title : {{ item.title }}</h1>
+        <br>
+        <h2> Content : {{ item.content }}</h2>
+        <br>
+        </div>
+        <div v-if="item.comments">
+            <p v-for = "dataitem in item.comments"> {{ dataitem.user }}</p>
+        </div>
+    </div>
+ </template>
+ 
+
+ 
---
+++ server/api/posts.get.js
@@ -0,0 +1,5 @@
+  import db from "../../db.json"
+  export default defineEventHandler((event)=>{
+    console.log(event)
+    return db.posts;
+  })
