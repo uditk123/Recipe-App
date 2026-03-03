3a5a9fa810af64d825230390af62684a1334d07d
Udit kumar <udit@updot.co>
2026-03-03 13:10:50 +0530

    Error Handle throw Fallback

--- app/pages/clientErroeHandle.vue
+++ app/pages/clientErroeHandle.vue
@@ -1,1 +1,13 @@
-  
+
+  <template>
+    <NuxtErrorBoundary>
+        <NuxtPage />
+        <template #error = "{ error }">
+            <div>
+                <h2>
+                    Error !: {{ error.statusMessage }}
+                </h2>
+            </div>
+        </template>
+    </NuxtErrorBoundary>
+  </template>
--- app/pages/clientErrorHandle/client.vue
+++ app/pages/clientErrorHandle/client.vue
@@ -1,11 +1,17 @@
   <script setup>
- 
+   const throwError = ()=>{
+       throw createError({
+        statusCode : 400,
+        statusMessage : "Page not found"
+       })
+   }
  </script>
  
  <template>
     <div>
       <h1> Learing Client Side Error handling using CreateError Method</h1>
       <p> Dummy Page for testing client Error</p>
+      <button @click = "throwError()"> Throw Error</button>
     </div>
  </template>
  
