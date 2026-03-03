c45682782840ef1fea76c5cecf1134a698774968
Udit kumar <udit@updot.co>
2026-03-03 12:48:14 +0530

    Error handling client side

---
+++ app/pages/clientErrorHandle/client.vue
@@ -0,0 +1,12 @@
+  <script setup>
+ 
+ </script>
+ 
+ <template>
+    <div>
+      <h1> Learing Client Side Error handling using CreateError Method</h1>
+      <p> Dummy Page for testing client Error</p>
+    </div>
+ </template>
+ 
+ 
